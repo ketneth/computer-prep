cff8afa9a3d7d2756edb4eaa2d6af9be19a90678
AlCpwnd <a.dejaeger@hotmail.com>
2024-08-14 16:23:32 +0200

    - Added a reboot as administrator if the script is ran as a standard user. - Added "prep" folder check and creation. - Added file copy.

--- 01.StartPreparation.bat
+++ 01.StartPreparation.bat
@@ -1,3 +1,20 @@
 @echo off
 
+REM Verifies if the script is running as admin.
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    goto Continue
+) else (
+    REM Restarts the script as admin.
+    powershell -command "Start-Process %~dpnx0 -Verb runas"
+)
+
+:Continue
+REM Checks if the "C:\Temp\prep" folder exists.
+IF NOT EXIST C:\Temp\prep MKDIR C:\Temp\prep
+
+REM Copies the contents of the current disk to the destination folder.
+pushd %~dp0
+COPY .\* C:\Setup
+
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\Temp\prep\scripts\001.PowerShell7.ps1"
