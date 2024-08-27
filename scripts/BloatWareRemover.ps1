588e639ed4b4db234f5eedc43c77a495fcfd7a64
AlCapwnd <a.dejaeger@hotmail.com>
2024-08-27 18:03:37 +0200

    Renamed original script and adapted:
    - Implemented "Add-LogMessage" function for streamlining logging
    - Added hardware manufacturer check with exit if not HP

--- scripts/BloatWareRemover.ps1
+++ scripts/BloatWareRemover.ps1
@@ -1,20 +1,44 @@
 #Requires -RunAsAdministrator
 
-$STName = "Task 3 Remove Bloatware"
-    # create a scheduled task with powershell for the next step
-    if (Get-ScheduledTask -TaskName $STName -ErrorAction Ignore) {
-        Write-Host "$STName exists."
-        Start-Sleep -Seconds 1.5
-        $Task = Get-ScheduledTask | Where-Object { $_.TaskName -eq $STName } | Select-Object -First 1
-        $Task | Unregister-ScheduledTask -Confirm:$false
-        Write-Host "$STName was removed" -ForegroundColor Yellow
-        Start-Sleep -Seconds 1.5
-    }
+param(
+    [Parameter(Mandatory)][String]$LogFile
+)
 
-# Creates a link in the startup folder.
+function Add-LogMessage{
+	param(
+		[Parameter(MandaTory, Position=0)][String]$LogFile,
+		[Parameter(Mandatory, ValueFromPipeline)][String]$Message
+	)
+	$Date = Get-Date -Format HH:MM:ss
+	"$Date`t$Message" | Out-File $LogFile -Append
+
+	<#
+        .SYNOPSIS
+        Add Message to LogFile.
+
+        .DESCRIPTION
+        Adds timestamp and Message to LogFile.
+    #>
+}
+
+# Documents the bloatware removal start time.
+"[BloatwareRemover Start]" | Add-LogMessage $LogFile
+
+# Recovering the device manufacturer.
+$Manufacturer = (Get-ComputerInfo -Property CsManufacturer).CsManufacturer
+
+# Stops the script is the device manufacturer is not HP.
+if($Manufacturer -notmatch "HP"){
+	"ERROR`tUnsupported Manufacturer: $Manufacturer" | Add-LogMessage $LogFile
+	"[BloatwareRemover End]" | Add-LogMessage $LogFile
+	return
+}
+
+# Location of the startup script.
 $Location = "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Restart.bat"
 
-$Script = 	"@echo off
+# Contents of the startup script.
+$Script = "@echo off
 REM Verifies if the script is running as admin.
 net session >nul 2>&1
 if %errorLevel% == 0 (
@@ -26,35 +50,35 @@ if %errorLevel% == 0 (
 powershell.exe -NoLogo -NoExit -NoProfile -ExecutionPolicy Bypass -File $PSCommandPath
 "
 
+# Creation of of the startup script.
 if(!(Test-Path $Location)){
 	$SCript | Out-File -FilePath $Location -Force
 }
 
-# Defines the manufacturer and exceptions for application removal.
-Write-Verbose "Recovering computer information..."
-$Manufacturer = (Get-ComputerInfo -Property CsManufacturer).CsManufacturer
+# Defines the exceptions for application removal.
 $Exceptions = "Realtek|Intel|Microsoft|Windows"
-Write-Verbose "$($Exceptions.Split("|").Count) exception(s) found:"
-$Exceptions.Split("|") | ForEach-Object{Write-Verbose "`t * $_"}
+"Configured Exceptions: $Exceptions" | Add-LogMessage $LogFile
 
-Write-Verbose "Recovering installed applications..."
+# Recovering matching applications.
 $BloatWareApps = Get-AppxPackage -AllUsers | Where-Object{$_.Name -notmatch $Exceptions -and $_.Name -match $Manufacturer}
+"Configured Exceptions: $Exceptions" | Add-LogMessage $LogFile
 
-Write-Host "Removing applications from the current profile..."
+# Removing them from the current users.
 $BloatWareApps | Remove-AppxPackage -AllUsers | Out-Null
-Write-Host "Removing applications from the template profile..."
+
+# Removing them from the new users profile.
 Get-AppxProvisionedPackage -Online | Where-Object{$_.DisplayName -match $Manufacturer -and $_.DisplayName -notmatch $Exceptions} | Remove-AppxProvisionedPackage -Online
 
+# Removing bloatware from the control panel applications.
 $RetryCount = 0
 While($RetryCount -lt 3 -or $Retries){
-	Write-Verbose "Recovering installed software..."
 	$Soft = Get-WmiObject -Class Win32_Product | Where-Object{$_.Vendor -match $Manufacturer -and $_.Name -notmatch $Exceptions}
-	Write-Host "Removing installed software..."
 	$i = 0
 	$iMax = $Soft.Count
 	$Retries = @()
 	foreach($SoftWare in $Soft){
 		Write-Progress -Activity 'Uninstalling' -Status $Software.Name -PercentComplete (($i/$iMax)*100)
+		"Uninstalling: $($Software.Name)" | Add-LogMessage $LogFile
 		$Temp = $SoftWare.Uninstall()
 		if($Temp.ReturnValue){
 			$Retries += $SoftWare
@@ -65,8 +89,8 @@ While($RetryCount -lt 3 -or $Retries){
 }
 
 if(!$Soft){
+	"[BloatwareRemover End]" | Add-LogMessage $LogFile
 	Remove-Item $Location
-	Write-Host "No bloatware found."
 }
 
 Restart-Computer
