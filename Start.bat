8b87e1e7a9bed49e290bbf8b65cf78fd8df1a4d5
AlCapwnd <a.dejaeger@hotmail.com>
2024-08-28 23:21:22 +0200

    Changes:
    - Added support for spaces within the serial number (will be replaced by uderscores in the file name)
    - Documents the start date of the script
    - Documents the original name of the device

--- Start.bat
+++ Start.bat
@@ -18,8 +18,15 @@ pushd %~dp0
 XCOPY .\* C:\Temp\prep /S /Y
 
 REM Creates a log file named after the serial number of the host computer.
-FOR /F "skip=1" %%G IN ('"wmic bios get serialnumber"') DO (ECHO %DATE%>C:\Temp\prep\%%G_log.txt)
-echo %~dp0 >> C:\Temp\prep\*_log.txt
+set command=wmic bios get serialnumber /format:value
+for /f "tokens=2 delims==" %%a in ('%command%') do set serial=%%a
+set serial=%serial: =_%
+REM Documents the date at which the script is ran.
+echo %DATE% >> C:\Temp\prep\%serial%_log.txt
+REM Documents the original hostname.
+echo DeviceName=%COMPUTERNAME% >> C:\Temp\prep\%serial%_log.txt
+REM Documents the original files' location.
+echo SourceLocation=%~dp0 >> C:\Temp\prep\%serial%_log.txt
 
 REM Runs the next script.
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\Temp\prep\scripts\001.PowerShell7.ps1"
