4c4b53f6030f58345cd1f713ad14641aa1067018
AlCapwnd <a.dejaeger@hotmail.com>
2024-08-29 21:37:47 +0200

    Removed the automatic script resrart part, as it will be handled by the 'Main.ps1' script.

--- scripts/BloatWareRemover.ps1
+++ scripts/BloatWareRemover.ps1
@@ -34,27 +34,6 @@ if($Manufacturer -notmatch "HP"){
 	return
 }
 
-# Location of the startup script.
-$Location = "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Restart.bat"
-
-# Contents of the startup script.
-$Script = "@echo off
-REM Verifies if the script is running as admin.
-net session >nul 2>&1
-if %errorLevel% == 0 (
-	goto Continue
-) else (
-	REM Restarts the script as admin.
-	powershell -command `"Start-Process %~dpnx0 -Verb runas`"
-)
-powershell.exe -NoLogo -NoExit -NoProfile -ExecutionPolicy Bypass -File $PSCommandPath
-"
-
-# Creation of of the startup script.
-if(!(Test-Path $Location)){
-	$SCript | Out-File -FilePath $Location -Force
-}
-
 # Defines the exceptions for application removal.
 $Exceptions = "Realtek|Intel|Microsoft|Windows"
 "Configured Exceptions: $Exceptions" | Add-LogMessage $LogFile
@@ -90,7 +69,6 @@ While($RetryCount -lt 3 -or $Retries){
 
 if(!$Soft){
 	"[BloatwareRemover End]" | Add-LogMessage $LogFile
-	Remove-Item $Location
 }
 
 Restart-Computer
