1b0971e3a999017f9bddf70410ceb0cff1ea0b07
AlCpwnd <a.dejaeger@hotmail.com>
2024-08-30 17:16:36 +0200

    Started implementing Local admin password change and cleanup.

--- scripts/Main.ps1
+++ scripts/Main.ps1
@@ -18,16 +18,17 @@ function Add-LogMessage{
 }
 
 # Imports the config file parameters.
-[xml]$Config = Get-Content $PSScriptRoot\Config.xml
+$ParentFolder = Split-Path $PSScriptRoot -Parent
+$Config = Get-Content -Path $ParentFolder\Config.json -Raw | ConvertFrom-Json
 
 # Defines log file name.
 $Serial = (wmic bios get serialnumber | Where-Object{$_})[1].Trim()
-$LogPath = "$PSScriptRoot\logs\$Serial`_log.txt"
+$LogPath = "$PSScriptRoot\$Serial`_log.txt"
 
 # Verifies if a corresponding file exists.
 if(-not (Test-Path -Path $LogPath -PathType Leaf)){
     $Serial = $Serial.Replace(" ","_")
-    $LogPath = "$PSScriptRoot\logs\$Serial`_log.txt"
+    $LogPath = "$PSScriptRoot\$Serial`_log.txt"
     # Verifies again, replacing spaces by underscores.
     if(-not (Test-Path -Path $LogPath -PathType Leaf)){
         # Creates the file if it cannot be found.
@@ -58,27 +59,26 @@ $LogFile = Get-Content -Path $LogPath
 
 <# Windows Updates #>
 $WindowsUpdateEnd = $LogFile | Where-Object{$_ -match "[WindowsUpdate End]"}
-if($Config.Settings.WindowsUpdate.Run -match "true" -and -not $WindowsUpdateEnd){
+if($Config.WindowsUpdate -match "true" -and -not $WindowsUpdateEnd){
     & $PSScriptRoot\WindowsUPdate.ps1 -LogFile $LogPath -Auto
 }
 
 <# Bloatware Removal #>
 $BloatwareRemovalEnd = $LogFile | Where-Object{$_ -match "[BloatwareRemover End]"}
-if($Config.Settings.BloatwareRemoval.Run -match "true" -and -not $BloatwareRemovalEnd){
+if($Config.BloatwareRemoval -and -not $BloatwareRemovalEnd){
     & $PSScriptRoot\BloatwareRemoval.ps1 -LogFile $LogPath
 }
 
 <# Computer Rename #>
 $ComputerRenameCheck = $LogFile | Where-Object{$_ -match "[Rename Skipped]|[Computer Renamed]"}
-if($Config.Settings.ComputerRename.Run -match "true" -and -not $ComputerRenameCheck){
+if($Config.ComputerRename.Run -and -not $ComputerRenameCheck){
     $ComputerName = HOSTNAME
-    $NewName = $Config.Settings.ComputerRename.NewName
-    $NameCheck = $ComputerName -eq $NewName
-    if(-not $NameCheck){
+    $NewName = $Config.ComputerRename.NewName
+    if(-not $ComputerName -eq $NewName){
         if($NewName){
             "[Computer Renamed]" | Add-LogMessage $LogPath
             "Computer New Name: $NewName"
-            Rename-Computer -NewName $Config.Settings.ComputerRename.NewName -Restart
+            Rename-Computer -NewName $Config.ComputerRename.NewName -Restart
         }else{
             $Check = $true
             while($Check){
@@ -97,4 +97,66 @@ if($Config.Settings.ComputerRename.Run -match "true" -and -not $ComputerRenameCh
             }
         }
     }
+}
+
+<# UserSettings #>
+$UserSettingsCheck = $LogFile | Where-Object{$_ -match "[UserSettings End]"}
+if($Config.UserSettings | Where-Object{$_} -and -not $UserSettingsCheck){
+    "[UserSettings Start]" | Add-LogMessage $LogPath
+    # Loads the registry profile.
+    REG LOAD HKLM\Default C:\Users\Default\NTUSER.DAT
+    if($Config.UserSettings.DesktopCleanup){
+        Remove-Item -Path $Env:USERPROFILE\Desktop\ -Filter "*.lnk"
+        "User Desktop Cleaned" | Add-LogMessage $LogPath
+    }
+    if($Config.UserSettings.PublicDesktopCleanup){
+        Remove-Item -Path $Env:PUBLIC\Desktop\ -Filter "*.lnk"
+        "Public Desktop Cleaned" | Add-LogMessage $LogPath
+    }
+    if($Config.UserSettings.RemoveTaskView){
+        New-ItemProperty -Path "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Value "0" -PropertyType Dword -Force
+        "Taskview Removed" | Add-LogMessage $LogPath
+    }
+    if($Config.UserSettings.RemoveWidgets){
+        New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa" -Value "0" -PropertyType Dword -Force
+        "Widgets Removed" | Add-LogMessage $LogPath
+    }
+    if($Config.UserSettings.RemoveChat){
+        New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarMn" -Value "0" -PropertyType Dword -Force
+        "Chat Icon Removed" | Add-LogMessage $LogPath
+    }
+    if($Config.UserSettings.MoveStartLeft){
+        New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Value "0" -PropertyType Dword -Force
+        "Taskbar Moved to the Left" | Add-LogMessage $LogPath
+    }
+    if($Config.UserSettings.RemoveSearch){
+        $Path = "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Search"
+        if(-not (Test-Path -Path $Path)){
+            New-Item -Path $Path -Force
+            "Registry key missing. Created: $Path"
+        }
+        New-ItemProperty -Path $Path -Value "0" -PropertyType Dword -Force
+        "Search Icon Removed" | Add-LogMessage $LogPath
+    }
+    if($Config.UserSettings.StartMorePins){
+        New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_Layout" -Value "1" -PropertyType Dword -Force
+        "More Pinning space added to Start menu" | Add-LogMessage $LogPath
+    }
+    "[UserSettings End]" | Add-LogMessage $LogPath
+}
+
+<# LocalAdminPassword #>
+$LocalAdminPasswordCheck = $LogFile | Where-Object{$_ -match "[LocalAdminPassword End]"}
+if($Config.LocalAdminPassword -and -not $LocalAdminPasswordCheck){
+    "[LocalAdminPassword Start]" | Add-LogMessage $LogPath
+    $OriginCheck = $LogFile | Where-Object{$_ -match "SourceLocation="}
+    if($OriginCheck){
+
+    }
+    "[LocalAdminPassword End]" | Add-LogMessage $LogPath
+}
+
+<# Cleanup #>
+if($Config.Cleanup -and (Test-Parth -Path "C:\Temp\prep\")){
+    
 }
