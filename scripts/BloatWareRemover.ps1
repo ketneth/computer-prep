cb340887aad159847141e51319396747ee648f23
Alexis De Jaeger <a.dejaeger@hotmail.com>
2025-07-24 22:32:26 +0200

    Rewrote script
    > Redefined variables for clarity.
    > Removed progress bar from the script. Meant for use in the background
    and was redundant with the log file output.

--- scripts/BloatWareRemover.ps1
+++ scripts/BloatWareRemover.ps1
@@ -1,18 +1,18 @@
 #Requires -RunAsAdministrator
 
 param(
-    [Parameter(Mandatory)][String]$LogFile
+    [Parameter(Mandatory)][String]$logPath
 )
 
 function Add-LogMessage{
 	param(
-		[Parameter(MandaTory, Position=0)][String]$logFile,
+		[Parameter(MandaTory, Position=0)][String]$logPath,
 		[Parameter(Mandatory, ValueFromPipeline)][String]$message
 	)
 
     process{
         $Date = Get-Date -Format HH:MM:ss
-        "$Date`t$message" | Out-File $logFile -Append -Encoding ascii
+        "$Date`t$message" | Out-File $logPath -Append -Encoding ascii
         Write-Verbose $message
     }
 
@@ -26,53 +26,72 @@ function Add-LogMessage{
 }
 
 # Documents the bloatware removal start time.
-"==BloatwareRemover Start==" | Add-LogMessage $LogFile
+"==Bloatware Removal Start==" | Add-LogMessage $logPath
 
 # Recovering the device manufacturer.
-$Manufacturer = (Get-ComputerInfo -Property CsManufacturer).CsManufacturer
+$manufacturer = (Get-ComputerInfo -Property CsManufacturer).CsManufacturer
 
-# Stops the script is the device manufacturer is not HP.
-if($Manufacturer -notmatch "HP"){
-	"ERROR`tUnsupported Manufacturer: $Manufacturer" | Add-LogMessage $LogFile
-	"==BloatwareRemover End==" | Add-LogMessage $LogFile
+# Finds the exceptions associated with the computer's manufacturer.
+# If none are found, the script quits.
+switch($manufacturer){
+	"HP"{$exceptions = "Realtek|Intel|Microsoft|Windows"}
+	"DELL"{}
+	"LENOVO"{}
+	default{}
+}
+
+# Stops the script if no exceptions are found for the given manufacturer.
+if($null -eq $exceptions){
+	"ERROR`tUnsupported Manufacturer: $manufacturer" | Add-LogMessage $logPath
+	"==Bloatware Removal End==" | Add-LogMessage $logPath
 	return
-}
-
-# Defines the exceptions for application removal.
-$Exceptions = "Realtek|Intel|Microsoft|Windows"
-"Configured Exceptions: $Exceptions" | Add-LogMessage $LogFile
-
-# Recovering matching applications.
-$BloatWareApps = Get-AppxPackage -AllUsers | Where-Object{$_.Name -notmatch $Exceptions -and $_.Name -match $Manufacturer}
-"Configured Exceptions: $Exceptions" | Add-LogMessage $LogFile
-
-# Removing them from the current users.
-$BloatWareApps | Remove-AppxPackage -AllUsers | Out-Null
-
-# Removing them from the new users profile.
-Get-AppxProvisionedPackage -Online | Where-Object{$_.DisplayName -match $Manufacturer -and $_.DisplayName -notmatch $Exceptions} | Remove-AppxProvisionedPackage -Online
-
-# Removing bloatware from the control panel applications.
-$RetryCount = 0
-While($RetryCount -lt 3 -or $Retries){
-	$Soft = Get-WmiObject -Class Win32_Product | Where-Object{$_.Vendor -match $Manufacturer -and $_.Name -notmatch $Exceptions}
-	$i = 0
-	$iMax = $Soft.Count
-	$Retries = @()
-	foreach($SoftWare in $Soft){
-		Write-Progress -Activity 'Uninstalling' -Status $Software.Name -PercentComplete (($i/$iMax)*100)
-		"Uninstalling: $($Software.Name)" | Add-LogMessage $LogFile
-		$Temp = $SoftWare.Uninstall()
-		if($Temp.ReturnValue){
-			$Retries += $SoftWare
+}else{
+	"Manufacturer found: $manufacturer", "Corresponding exceptions: $exceptions" | Add-LogMessage $logPath
+	
+	# Recovering matching applications.
+	$bloatwareApps = Get-AppxPackage -AllUsers | Where-Object{$_.Name -notmatch $exceptions -and $_.Name -match $manufacturer}
+	"$($bloatwareApps.Count) app(s) found.", "Attempting uninstall." | Add-LogMessage $logPath
+	
+	# Removing them from the current users.
+	$bloatwareApps | Remove-AppxPackage -AllUsers | Out-File $logPath -Append
+	
+	# Removing them from the new users profile.
+	$provisionedBloatware =  Get-AppxProvisionedPackage -Online | Where-Object{$_.DisplayName -match $manufacturer -and $_.DisplayName -notmatch $exceptions}
+	"$($provisionedBloatware.Count) provisioned app(s) found.", "Attempting uninstall." | Add-LogMessage $logPath
+	$provisionedBloatware | Remove-AppxProvisionedPackage -Online | Out-File $logPath -Append
+	
+	# Removing bloatware from the control panel applications.
+	$retryCount = 0
+	While($retryCount -lt 3 -or $retries){
+		if($PSVersionTable.PSVersion.Major -eq 7){
+			$softwares = Get-CimInstance -ClassName Win32_Product
+		}else{
+			$softwares = Get-WmiObject -ClassName Win32_Product
 		}
-		$i++
+		$soft = $softwares | Where-Object{$_.Vendor -match $manufacturer -and $_.Name -notmatch $exceptions}
+		$retries = @()
+		foreach($software in $soft){
+			"Uninstalling: $($software.Name)" | Add-LogMessage $logPath
+			$Temp = $software.Uninstall()
+			if($Temp.ReturnValue){
+				"Failed uninstalling, retrying." | Add-LogMessage $logPath
+				$retries += $software
+			}else{
+				"Successfully uninstalled." | Add-LogMessage $logPath
+			}
+			$i++
+		}
+		$retryCount++
+		if($retries){
+			"Retrying for $($retries.Count) software(s), for $retryCount time(s)." | Add-LogMessage $logPath
+		}
 	}
-	$RetryCount++
+	if(-not ($bloatwareApps -and $provisionedBloatware -and $soft)){
+		"All bloatware removed" | Add-LogMessage $logPath
+	}else{
+		"Failed to remove all software.", "$($retries.Count) application(s) still present." | Add-LogMessage $logPath
+		$retries.Name | Out-File $logPath -Append
+	}
+	"==Bloatware Removal End==" | Add-LogMessage
+	Restart-Computer -Force
 }
-
-if(!$Soft){
-	"==BloatwareRemover End==" | Add-LogMessage $LogFile
-}
-
-Restart-Computer -Force
